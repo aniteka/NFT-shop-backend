3db4fff04d9cade43f14e6ba81a731896f3034ba
aniteka <an1teka4@gmail.com>
2024-04-26 12:13:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+const main = async function () {
+    try {
+        app.listen(PORT, function () {
+            console.log(`Listen on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+main()
