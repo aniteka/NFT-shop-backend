dd3f50e09c45c73be1d22b6b72c8ef9237b6b447
aniteka <an1teka4@gmail.com>
2024-05-01 21:18:17 +0300

    feat: add cors

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const authRouter = require('./routers/authRouter')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRouter)
 
